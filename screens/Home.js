a0847018531e2a246063422dd7fd6ec78a04dcb0
Razaul00003 <mdrazaulkarim00003@gmail.com>
2023-01-15 22:47:34 +0600

    added category compnent

--- screens/Home.js
+++ screens/Home.js
@@ -2,6 +2,7 @@ import { SafeAreaView, Text, View } from "react-native";
 import React from "react";
 import HeaderTabs from "../components/HeaderTabs";
 import SearchBar from "../components/SearchBar";
+import Categories from "../components/Categories";
 
 const Home = () => {
   return (
@@ -16,6 +17,7 @@ const Home = () => {
         <HeaderTabs />
         <SearchBar />
       </View>
+      <Categories />
     </SafeAreaView>
   );
 };
